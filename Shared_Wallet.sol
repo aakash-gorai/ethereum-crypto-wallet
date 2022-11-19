61ceb8aa58e660e9e9b5248513343f8acfa6cc46
Aakash <aakashkumargorai@gmail.com>
2022-11-20 02:26:43 +0530

    initial commit

---
+++ Shared_Wallet.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "./Allowance.sol";
+
+contract simpleWallet is Allowance{
+    event BalanceChanged(address indexed _transactionAc,uint _newBalance);
+
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+        require(_amount <= address(this).balance,"There is not enough fund available");
+        if(!isOwner()){
+            reduceAllowance(_msgSender(),_amount);
+        }
+        emit BalanceChanged(_to,_amount);
+        _to.transfer(_amount);
+    }
+
+    fallback () external payable {
+    emit BalanceChanged(_msgSender(),msg.value);
+    }
+}
