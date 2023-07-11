dcc15a9c498e477367d8dc7470d0fc11b8714a89
Aakash Kumar Gorai <aakashkumargorai@gmail.com>
2023-07-11 17:22:55 +0530

    Update Allowance.sol

--- Allowance.sol
+++ Allowance.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
-import "https://github.com/aakash-kumar-gorai/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Allowance is Ownable{
-    using SafeMath for uint;
+    
     event AllowanceChanged(address indexed _byWhom, address indexed _forWhom, uint _oldAmount, uint _newAmount);
     mapping(address => uint) public allowance;
 
@@ -14,18 +13,17 @@ contract Allowance is Ownable{
         allowance[_who] = _amount;
     }
 
-    // function isOwner()public view returns(bool){
-    //     return _msgSender() == owner();
-    // }
+    function isOwner()public view returns(bool){
+        return _msgSender() == owner();
+    }
 
     modifier ownerOrAllowed(uint _amount) {
-        
         require(isOwner() || allowance[_msgSender()] >= _amount,"You are not allowed");
         _;
     }
 
     function reduceAllowance(address _who, uint _amount) internal{
-        emit AllowanceChanged(_msgSender(), _who, allowance[_who], allowance[_who].sub(_amount));
-        allowance[_who]=allowance[_who].sub(_amount);
+        emit AllowanceChanged(_msgSender(), _who, allowance[_who], allowance[_who]-(_amount));
+        allowance[_who]=allowance[_who]-(_amount);
     }
 }
